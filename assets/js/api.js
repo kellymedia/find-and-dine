6ebef6ce58997d0a58f1b5ae41545fabe3d88a43
Michael Kelly <mk@uconn.edu>
2021-02-10 18:50:02 -0500

    update api.js
    get photo by id:1435899

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 console.log ("Js file is loaded")
 
 $.ajax({
-    url: "https://api.pexels.com/v1/search?query=food&size=large",
+    url: "https://api.pexels.com/v1/photos/1435899",
     headers: { "Authorization" : "563492ad6f917000010000015eb8fc8048ae495794945651de698734" }
 }).then(function(data){
     console.log('KitchenPics', data)
