ca460ff2864c5fc54de41bfee5046d0b3935d61d
Michael Kelly <mk@uconn.edu>
2021-02-10 19:18:08 -0500

    update api.js
    remove fixed css property for heroPic, remove duplicate center css property

--- assets/js/api.js
+++ assets/js/api.js
@@ -8,7 +8,7 @@ $.ajax({
     console.log('KitchenPics', data.photos[0].src.large)
     console.log('this our random num ??', Math.floor( Math.random() * data.photos.length))
     var randomNum = Math.floor( Math.random() * data.photos.length)
-    $(`<style type='text/css'> .heroPic{ background: url(${data.photos[randomNum].src.large}) no-repeat center center fixed; background-size: 100%; } </style>`).appendTo("head");
+    $(`<style type='text/css'> .heroPic{ background: url(${data.photos[randomNum].src.large}) no-repeat center; background-size: 100%; } </style>`).appendTo("head");
 
     var pic = $('<img>')
     pic.attr('src', data.photos[0].src.large)
